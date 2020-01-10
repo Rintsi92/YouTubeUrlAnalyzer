6f00f5a2c36fcd494065a5afa0ddc1c84f1c8dcd
Rintsi92 <joni.rinta-kahila@hotmail.com>
2020-01-10 02:11:13 +0200

    Add files via upload

---
+++ yttest/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace YouYubeLinkTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.UrlBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UrlLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBoxYT = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Copy = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYT)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UrlBox
+            // 
+            this.UrlBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.UrlBox.Location = new System.Drawing.Point(542, 20);
+            this.UrlBox.Name = "UrlBox";
+            this.UrlBox.Size = new System.Drawing.Size(443, 20);
+            this.UrlBox.TabIndex = 3;
+            this.UrlBox.TextChanged += new System.EventHandler(this.UrlBox_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnKeystroke;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1444, 420);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseClick);
+            this.dataGridView1.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.dataGridView1_RowPrePaint);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Gray;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Location = new System.Drawing.Point(1389, 20);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(41, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.BlueViolet;
+            this.btnSearch.Enabled = false;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(1069, 20);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(231, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 30);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "You ";
+            // 
+            // UrlLabel
+            // 
+            this.UrlLabel.AutoSize = true;
+            this.UrlLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UrlLabel.ForeColor = System.Drawing.Color.White;
+            this.UrlLabel.Location = new System.Drawing.Point(427, 23);
+            this.UrlLabel.Name = "UrlLabel";
+            this.UrlLabel.Size = new System.Drawing.Size(109, 16);
+            this.UrlLabel.TabIndex = 13;
+            this.UrlLabel.Text = "YouTube URL:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(57, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 30);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Tube";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(124, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 30);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "URL Analyzer";
+            // 
+            // pictureBoxYT
+            // 
+            this.pictureBoxYT.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBoxYT.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxYT.Image")));
+            this.pictureBoxYT.Location = new System.Drawing.Point(457, 96);
+            this.pictureBoxYT.Name = "pictureBoxYT";
+            this.pictureBoxYT.Size = new System.Drawing.Size(528, 364);
+            this.pictureBoxYT.TabIndex = 16;
+            this.pictureBoxYT.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Copy});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // Copy
+            // 
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(180, 22);
+            this.Copy.Text = "Copy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(1442, 472);
+            this.Controls.Add(this.pictureBoxYT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UrlLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.UrlBox);
+            this.Name = "Form1";
+            this.Text = "YT-URL-ANALYZER";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYT)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox UrlBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label UrlLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBoxYT;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Copy;
+    }
+}
+
---
+++ yttest/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace YouYubeLinkTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const string YoutubeLinkRegex = "(?:.+?)?(?:\\/v\\/|watch\\/|\\?v=|\\&v=|youtu\\.be\\/|\\/v=|^youtu\\.be\\/)([a-zA-Z0-9_-]{11})+"; // Annetun linkin validointi
+
+        internal static string ValidateVideoLink(string input)
+        {
+            var regex = new Regex(YoutubeLinkRegex, RegexOptions.Compiled);
+            foreach (Match match in regex.Matches(input))
+            {
+                //Console.WriteLine(match);
+                foreach (var groupdata in match.Groups.Cast<Group>().Where(groupdata => !groupdata.ToString().StartsWith("http://") && !groupdata.ToString().StartsWith("https://") && !groupdata.ToString().StartsWith("youtu") && !groupdata.ToString().StartsWith("www.")))
+                {
+                    return groupdata.ToString();
+                }
+            }
+            return string.Empty;
+        }
+
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e) // Datan sorttaaminen datagridin headeria klikkaamalla
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                DataGridViewColumn newColumn =
+                dataGridView1.Columns[e.ColumnIndex];
+
+                SortOrder direction;
+                if (dataGridView1.SortOrder == SortOrder.Ascending)
+                {
+                    dataGridView1.Sort(newColumn,
+                    ListSortDirection.Ascending);
+                    direction = SortOrder.Ascending;
+                }
+                else
+                {
+                    dataGridView1.Sort(newColumn,
+                    ListSortDirection.Descending);
+                    direction = SortOrder.Descending;
+                }
+                newColumn.HeaderCell.SortGlyphDirection = direction;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e) // DataGridin tyhjentäminen tapahtuu tässä
+        {
+            int numRows = dataGridView1.Rows.Count;
+            for (int i = 0; i < numRows; i++)
+            {
+                try
+                {
+                    int max = dataGridView1.Rows.Count - 1;
+                    dataGridView1.Rows.Remove(dataGridView1.Rows[max]);
+                    pictureBoxYT.Visible = true;
+                }
+                catch (Exception exe)
+                {
+                    MessageBox.Show("You can´t delete A row " + exe, "WTF",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)  // Tehdään tietopyyntö linkistä ja käsitellään virheet
+        {
+            pictureBoxYT.Visible = false;
+            try
+            {
+                var list = YouTube.Default.GetAllVideos(string.Format(UrlBox.Text));
+                dataGridView1.DataSource = new System.Collections.ObjectModel.ObservableCollection<YouTubeVideo>(list).ToBindingList();
+            }
+            catch (Exception exe)
+            {
+                MessageBox.Show("There may be a problem with your URL " + exe, "WTF",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void UrlBox_TextChanged(object sender, EventArgs e)  //Testataan että YouTube Linkki vaikuttaa oikealta
+        {
+            if (ValidateVideoLink(UrlBox.Text) != string.Empty)
+            {
+                btnSearch.Enabled = true;
+                while(this.Height == 510)
+                {
+                    this.Height++;
+                }
+            }
+            else
+            {
+                btnSearch.Enabled = false;
+            }
+        }
+
+        private void dataGridView1_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            if ((Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["Resolution"].Value) < 1) && Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["AudioBitrate"].Value) < 1)
+            {
+                dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.LightPink;
+            }
+            else if((Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["Resolution"].Value) > 1) && Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["AudioBitrate"].Value) > 1)
+            {
+                dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.LightGreen;
+            }
+            else if ((Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["Resolution"].Value) > 1) && Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["AudioBitrate"].Value) == -1)
+            {
+                dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.LightCyan;
+            }
+            else if ((Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["Resolution"].Value) == -1) && Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["AudioBitrate"].Value) > 1)
+            {
+                dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.Lime;
+            }
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem.Text == "Copy" && dataGridView1.CurrentCell.Value != null)
+            {
+                Clipboard.SetDataObject(dataGridView1.CurrentCell.Value.ToString(), false);
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (/*e.Button == MouseButtons.Right &&*/ e.ColumnIndex != -1 && e.RowIndex != -1)
+            {
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].ContextMenuStrip = contextMenuStrip1;
+                dataGridView1.CurrentCell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            }
+        }
+    }
+}
